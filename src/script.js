a0b91194b6dcff9544198474cba8f8b331aed4cd
paulgoblin <mpr75@cornell.edu>
2022-09-18 22:36:11 -0700

    Add npm-watch

---
+++ src/script.js
@@ -0,0 +1,15 @@
+var pug = require('pug');
+const fs = require('fs');
+const path = require('path');
+
+const INDEX_INPUTS = {
+    pageTitle: 'Jessy Lu'
+};
+
+const BUILD_DIR = path.resolve(__dirname, "../build");
+
+const indexPage = pug.renderFile(path.resolve(__dirname, 'templates/index.pug'), INDEX_INPUTS);
+
+fs.writeFileSync(path.resolve(BUILD_DIR, 'index.html'), indexPage, console.log);
+
+
