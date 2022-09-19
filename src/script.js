95cf5955d71f2ecab09a2b5ad65779dd6d22340b
paulgoblin <mpr75@cornell.edu>
2022-09-18 23:00:23 -0700

    Add airbnb eslint

--- src/script.js
+++ src/script.js
@@ -1,15 +1,13 @@
-var pug = require('pug');
+const pug = require('pug');
 const fs = require('fs');
 const path = require('path');
 
 const INDEX_INPUTS = {
-    pageTitle: 'Jessy Lu'
+  pageTitle: 'Jessy Lu',
 };
 
-const BUILD_DIR = path.resolve(__dirname, "../build");
+const BUILD_DIR = path.resolve(__dirname, '../build');
 
 const indexPage = pug.renderFile(path.resolve(__dirname, 'templates/index.pug'), INDEX_INPUTS);
 
-fs.writeFileSync(path.resolve(BUILD_DIR, 'index.html'), indexPage, console.log);
-
-
+fs.writeFileSync(path.resolve(BUILD_DIR, 'index.html'), indexPage, console.error);
